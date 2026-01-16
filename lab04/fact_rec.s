fd329aff55912565fe06742cd2687188112d45c6
wirthlin <wirthlin@byu.edu>
2026-01-16 11:37:47 -0700

    Updates to factorial example

--- lab04/fact_rec.s
+++ lab04/fact_rec.s
@@ -21,27 +21,27 @@
 # The function needs to be in the text segment
 .text
 
-fact_rec:						# Performs factorial for input a0 (a0!) and returns result to a0
+fact_rec:			# Performs factorial for input a0 (a0!) and returns result to a0
 
-	addi sp, sp, -8				# Make room to save values on the stack
-	sw s0, 0(sp)				# Save the caller s0 on stack. Used as the subroutine factorial operand
-	sw ra, 4(sp)				# The return address needs to be saved to know where subroutine was called from 
+	addi sp, sp, -8		# fact_rec: Make room to save values on the stack
+	sw s0, 0(sp)		# Save the caller s0 on stack. Used as the subroutine factorial operand
+	sw ra, 4(sp)		# The return address needs to be saved to know where subroutine was called from 
 
-	mv s0, a0					# Save the argument into s0 (Used to compute the next factorial operand)
+	mv s0, a0		# Save the argument into s0 (Used to compute the next factorial operand)
 
-	bgtz a0,$L2					# Branch if input > 0 there are additional factorial operands that still needs to be stored
-	li a0,1						# Return 1, input must be 0 so set the operand to 1 since input = 0, result = 1
-	j $L1						# Jump to code to return (end of recursion)
+	bgtz a0,$L2		# Branch if input > 0 there are additional factorial operands that still needs to be stored
+	li a0,1			# Return 1, input must be 0 so set the operand to 1 since input = 0, result = 1
+	j $L1			# Jump to code to return (end of recursion)
 
 $L2:
-	addi a0,a0,-1				# Compute n - 1
-	jal fact_rec				# Call factorial function to store next factorial operand, argument (a0 (input)) return value (a0)
-	mul a0,a0,s0				# All factorial operands have been stored, multiple current stored operand and
-								# return operand to Compute fact(input-1) * input 
-	   
-$L1:    
-	lw s0, 0(sp)				# Restore any callee saved regs used. Load previous callee factorial operand
-	lw ra, 4(sp)				# Restore return address
-	addi sp, sp, 8				# Update stack pointer
+	addi a0,a0,-1		# Compute n - 1
+	jal fact_rec		# Call factorial function to store next factorial operand, argument (a0 (input)) return value (a0)
+	mul a0,a0,s0		# All factorial operands have been stored, multiple current stored operand and
+				# return operand to Compute fact(input-1) * input 
 
-	ret							# Jump to return address
+$L1:    
+	lw s0, 0(sp)		# Restore any callee saved regs used. Load previous callee factorial operand
+	lw ra, 4(sp)		# Restore return address
+	addi sp, sp, 8		# Update stack pointer
+
+	ret			# Jump to return address
--- lab04/fact_rec_main.s
+++ lab04/fact_rec_main.s
@@ -12,10 +12,10 @@
 .globl  main
 
 # Constant defines for system calls
-.eqv PRINT_INT 1
-.eqv PRINT_STR 4
-.eqv READ_INT 5
-.eqv EXIT_CODE 93
+.eqv PRINT_INT 1        
+.eqv PRINT_STR 4        
+.eqv READ_INT 5        
+.eqv EXIT_CODE 93      
 
 
 # Global data segment
@@ -39,38 +39,38 @@ eol_str:			# End of line
 .text
 main:				# Label for start of program
 
-# Print the input prompt
+	# Print the input prompt
 	la a0,input_str
 	li a7,PRINT_STR
 	ecall
+
 	# System call to read integer input from user
 	li a7,READ_INT
 	ecall			# Result is in a0
 	la t0, input		# Load address of input
 	sw a0, 0(t0)		# Store the user input (a0) value into memory
 
+	# Call the fact_rec function to compute the factorial
 	lw a0,input		# Loads the desired input value from memory to compute the factorial
 	jal fact_rec		# Jump and link (save return address) to factorial subroutine (function), argument (a0 (input)) return value 
+
 	la t0,output		# Load output address to t0
 	sw a0,0(t0)		# Save the calculated factorial result to output memory location
 	
-exit:				# The factorial has finished computing, perform system calls to print
-				# result and waits on debug breakpoint
 	lw a0,input		# Load Input value into a0 to be printed
 	li a7,PRINT_INT		# System call code for print_int code 1
-	ecall			# Make system call
-		
+	ecall			# Make system call		
 
 	la a0,result_str	# Put result_str address in a0 to be printed
 	li a7,PRINT_STR		# System call code for print_str code 4
 	ecall			# Make system call
  
 	lw a0,output		# Load output value into a0 to be printed
-	li a7,PRINT_INT		# System call code for print_int code 1
+	li a7,PRINT_INT	# System call code for print_int code 1
 	ecall			# Make system call
 
 	la a0,eol_str		# Put end of line
-	li a7,PRINT_STR		# System call code for print_str code 4
+	li a7, PRINT_STR	# System call code for print_str code 4
 	ecall			# Make system call (i.e., print string)
 	
 	li a0,0			# Exit (93) with code 0
